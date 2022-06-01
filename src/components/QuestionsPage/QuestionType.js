6624509f17b8aae7376038f0e623f742fa1fecc9
19jzwiener <73654309+19jzwiener@users.noreply.github.com>
2022-06-01 14:58:40 -0500

    Workout Comments & QuestionType mapping
    Changed comments on Workout Page
    
    Copied Workout mapping into QuestionType to create similar activity

--- src/components/QuestionsPage/QuestionType.js
+++ src/components/QuestionsPage/QuestionType.js
@@ -1,39 +1,124 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Personal from "./Personal";
 
 import "./QuestionType.css";
 
 function QuestionType() {
+  // List of all types of questions
+  const [type, setType] = useState([]);
 
+  // UseState to create more types
+  const [typeHeader, setTypeHeader] = useState("");
 
+  // UseState to create questions under each type
+  const [typeQuestions, setTypeQuestions] = useState("");
 
-  const [type, setType] = useState([
-    "Motivational" 
-  ]);
+  // UseState for index --> will be added later
+  const [addIndex, setAddIndex] = useState(null);
+
+  useEffect(() => {
+    const data = window.localStorage.getItem("TYPE_INFO");
+    if (data !== null) setType(JSON.parse(data));
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("TYPE_INFO", JSON.stringify(type));
+  }, [type]);
 
   return (
     <div className="type">
-     
-
       <div>
         {type.map((questionType, index) => {
           return (
-            <article >
+            <article>
               <div className="sidebar__box">
                 <div className="sidebar__header">
-                  <p>{questionType} questions</p>
-                  <button className="sidebar__btnSmall">ADD</button>
+                  <p>{questionType.typeHeader} questions</p>
                 </div>
-                
+                {questionType.questions.map((question, index) => {
+                  return (
+                    // Take questions stuff from other page
+                    <div>{question}</div>
+                  );
+                })}
+                <div>
+                  <button>delete question type</button>
+                  {index == addIndex ? (
+                    <>
+                      {/* Add questions to specific questionType */}
+                      <input
+                        type="text"
+                        name="TypeQuestion"
+                        value={typeQuestions}
+                        onChange={(e) => setTypeQuestions(e.target.value)}
+                      />
+                      <button
+                        onClick={() => {
+                          // Make sure question isn't empty
+                          if (typeQuestions.length) {
+                            let sectionToAddQuestionTo = type.slice(index)[0];
+                            // push type info to typeQuestions
+                            sectionToAddQuestionTo.questions.push(
+                              typeQuestions
+                            );
+                            // Place new question at end of array
+                            setType([
+                              ...type.slice(0, index),
+                              sectionToAddQuestionTo,
+                              ...type.slice(index + 1),
+                            ]);
+                            // reset description to blank
+                            setTypeQuestions("");
+                          }
+                        }}
+                      >
+                        Enter Question
+                      </button>
+                    </>
+                  ) : (
+                    <button onClick={() => setAddIndex(index)}>
+                      Add Question
+                    </button>
+                  )}
+                </div>
               </div>
               <div className="sidebar__lineBreak" />
             </article>
           );
         })}
-      </div>
 
-      
+        <div>
+          <input
+            type="text"
+            name="typeHeader"
+            value={typeHeader}
+            onChange={(e) => setTypeHeader(e.target.value)}
+          />
+          <button
+            className=""
+            onClick={() => {
+              // make sure typeHeader isn't empty
+              if (typeHeader.length) {
+                // take current header list and add new header
+                setType([
+                  ...type,
+                  {
+                    // Still don't quite understand ask Ben or Preston
+                    typeHeader: typeHeader,
+                    // create new questions array
+                    questions: [],
+                  },
+                ]);
+                // reset typeHeader to blank
+                setTypeHeader("");
+              }
+            }}
+          >
+            Enter Header Cell
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/leftSidebarComp/Workout.js
+++ src/components/leftSidebarComp/Workout.js
@@ -53,7 +53,7 @@ function Workout() {
                 </div>
 
                 {/* Creating new array under row.workouts */}
-                {/* Iterating over row.workouts array */}
+                {/* Iterating over row.workouts array --> created below!! */}
                 {row.workouts.map((workout, index) => {
                   return <td className="homeWorkout__cell">{workout}</td>;
                 })}
@@ -65,8 +65,10 @@ function Workout() {
                     >
                       Delete Row
                     </button>
+                    {/* change add workout box depending on what index has been clicked */}
                     {index == addIndex ? (
                       <>
+                      {/* Add workouts to specific row */}
                         <input
                           type="text"
                           name="description"
@@ -76,7 +78,7 @@ function Workout() {
                         <button
                           className="homeWorkout__btn"
                           onClick={() => {
-                            // make sure todo isn't empty
+                            // make sure new workout isn't empty
                             if (descriptionBox.length) {
                               //
                               let rowToAddWorkoutTo = workoutRows.slice(index)[0];
@@ -115,14 +117,15 @@ function Workout() {
             <button
               className=""
               onClick={() => {
-                // make sure todo isn't empty
+                // make sure descriptionHeader isn't empty
                 if (descriptionHeader.length) {
-                  // take current task list and add new task at end
+                  // take current task list and add new header at end
                   setWorkoutRows([
                     ...workoutRows,
                     {
                       // ????? set typed info to description
                       descriptionHeader: descriptionHeader,
+                      // Create workouts array
                       workouts: [],
                     },
                   ]);

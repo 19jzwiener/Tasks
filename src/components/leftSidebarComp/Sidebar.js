3575911b049e2c3913171aaa46e8888f7bee9c91
19jzwiener <73654309+19jzwiener@users.noreply.github.com>
2022-07-02 14:03:24 -0500

    Workout Bootstrap change
    Changed table to div columns and rows using bootstrap preset classes.

--- src/components/leftSidebarComp/Sidebar.js
+++ src/components/leftSidebarComp/Sidebar.js
@@ -19,79 +19,82 @@ function Sidebar(props) {
 
   return (
     <div className="app">
-      <div className="sidebar">
-        {/* top box for the left sidebar */}
-        {/* Holds users name and a question for what someone would like to do */}
-        <div className="sidebar__header">
-          <div className="greeting">Joseph Zwiener</div>
-          <div className="select">What to do?</div>
+      <div className="container">
+        <div className="row">
+          <div className=" sidebar">
+            {/* top box for the left sidebar */}
+            {/* Holds users name and a question for what someone would like to do */}
+            <div className="sidebar__header">
+              <div className="greeting">Joseph Zwiener</div>
+              <div className="select">What to do?</div>
+            </div>
+            <Link to="/Questions">Questions Page</Link>
+            {/* linebreak between different sets of information */}
+            <div className="sidebar__lineBreak" />
+
+            {/* popout window buttons */}
+            <div className="sidebar__section">
+              {/* open and close task popout */}
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  setTaskOpen(!taskOpen);
+                  setWorkoutOpen(false);
+                  setWeatherOpen(false);
+                }}
+              >
+                Tasks
+              </button>
+
+              {/* open and close workout popout */}
+              <button
+                className="btn btn-success"
+                onClick={() => {
+                  setWorkoutOpen(!workoutOpen);
+                  setTaskOpen(false);
+                  setWeatherOpen(false);
+                }}
+              >
+                Workouts
+              </button>
+
+              {/* open and close weather */}
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  setWeatherOpen(!weatherOpen);
+                  setWorkoutOpen(false);
+                  setTaskOpen(false);
+                }}
+              >
+                Weather
+              </button>
+            </div>
+
+            {/* linebreak between sections */}
+            <div className="sidebar__lineBreak" />
+          </div>
+          {/* if workout is true do such, if closed null */}
+          {workoutOpen ? (
+            <div >
+              <Workout />
+            </div>
+          ) : null}
+
+          {/* if taskOpen is true do such, if closed null */}
+          {taskOpen ? (
+            <div>
+              <Task />
+            </div>
+          ) : null}
+
+          {/* if weatherOpen is true do such, if closed null */}
+          {weatherOpen ? (
+            <div>
+              <Weather />
+            </div>
+          ) : null}
         </div>
-        <Link to="/Questions">Questions Page</Link>
-        {/* linebreak between different sets of information */}
-        <div className="sidebar__lineBreak" />
-
-        {/* popout window buttons */}
-        <div className="sidebar__section">
-          {/* open and close task popout */}
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              setTaskOpen(!taskOpen);
-              setWorkoutOpen(false);
-              setWeatherOpen(false);
-            }}
-          >
-            Tasks
-          </button>
-
-          {/* open and close workout popout */}
-          <button
-            className="btn btn-success"
-            onClick={() => {
-              setWorkoutOpen(!workoutOpen);
-              setTaskOpen(false);
-              setWeatherOpen(false);
-            }}
-          >
-            Workouts
-          </button>
-
-          {/* open and close weather */}
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              setWeatherOpen(!weatherOpen);
-              setWorkoutOpen(false);
-              setTaskOpen(false);
-            }}
-          >
-            Weather
-          </button>
-        </div>
-
-        {/* linebreak between sections */}
-        <div className="sidebar__lineBreak" />
-
-        {/* if workout is true do such, if closed null */}
-        {workoutOpen ? (
-          <div className="sidebar__HomeWorkout">
-            <Workout />
-          </div>
-        ) : null}
-
-        {/* if taskOpen is true do such, if closed null */}
-        {taskOpen ? (
-          <div>
-            <Task />
-          </div>
-        ) : null}
-
-        {/* if weatherOpen is true do such, if closed null */}
-        {weatherOpen ? (
-          <div>
-            <Weather />
-          </div>
-        ) : null}
       </div>
     </div>
   );
--- src/components/leftSidebarComp/Workout.js
+++ src/components/leftSidebarComp/Workout.js
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import "./Workout.css";
 
 function Workout() {
-
   // create Rows for workouts to be added to
   const [workoutRows, setWorkoutRows] = useState([]);
 
@@ -13,9 +12,9 @@ function Workout() {
 
   // Allow for creation of Headers
   const [descriptionHeader, setDescriptionHeader] = useState("");
-  
+
   // ALlow selection of which row we want to add a workout too
-  const [addIndex, setAddIndex] = useState(null)
+  const [addIndex, setAddIndex] = useState(null);
 
   // save workoutRows input into localStorage
   useEffect(() => {
@@ -31,7 +30,7 @@ function Workout() {
     );
   }, [workoutRows]);
 
-  // delete workoutRow 
+  // delete workoutRow
   function deleteWorkoutRow(i) {
     console.log(JSON.parse(window.localStorage.getItem("WORKOUT_ROW_INFO"))[i]);
 
@@ -39,119 +38,123 @@ function Workout() {
   }
 
   return (
-    <div className="homeWorkout">
+    <div className="container">
       {/* table for workout */}
-      <table>
-        {/* header for type of workouts and then workouts */}
-        <tr className="homeWorkout__header">
-          <th className="homeWorkout__headerLeft">Type</th>
-          <th className="homeWorkout__headerRight">Workouts</th>
-        </tr>
-        <tbody>
-          {/* Iterating over the entire workoutRows array */}
-          {workoutRows.map((row, index) => {
-            return (
-              <tr className="homeWorkout__row">
-                <div>
-                  <td className="homeWorkout__cellHeader">
-                    {row.descriptionHeader}
-                  </td>
-                </div>
+      <div className="row homeWorkout__header">
+        <div className="col-2 text">Type</div>
+        <div className="col-8 text">Workouts</div>
+        <div className="col-1 text">Delete Row</div>
+        <div className="col-1 text">Add Workout</div>
+      </div>
 
-                {/* Creating new array under row.workouts */}
-                {/* Iterating over row.workouts array --> created below!! */}
-                {row.workouts.map((workout, index) => {
-                  return <td className="homeWorkout__cell">{workout}</td>;
-                })}
-                <div>
-                  <td className="homeWorkout__subDel">
-                    {/* delete whole workoutRow index */}
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => deleteWorkoutRow(index)}
-                    >
-                      Delete Row
-                    </button>
-                    {/* change add workout box depending on what index has been clicked */}
-                    {index == addIndex ? (
-                      <>
-                      {/* Add workouts to specific row */}
-                        <input
-                        className="form-control input-lg"
-                          type="text"
-                          name="description"
-                          value={descriptionBox}
-                          onChange={(e) => setDescriptionBox(e.target.value)}
-                        />
+      {/* Iterating over the entire workoutRows array */}
+      {workoutRows.map((row, index) => {
+        return (
+          <div className="row">
+            <div className="homeWorkout__cellHeader col-2">
+              {row.descriptionHeader}
+            </div>
 
-                        {/* add workout based off of input  */}
-                        <button
-                          className="btn btn-success"
-                          onClick={() => {
-                            // make sure new workout isn't empty
-                            if (descriptionBox.length) {
-                              //
-                              let rowToAddWorkoutTo = workoutRows.slice(index)[0];
-                              //push WorkoutRows information to descriptionBox
-                              rowToAddWorkoutTo.workouts.push(descriptionBox);
-                              // Place new workout at end of array ---- take workoutRows Array first index
-                              setWorkoutRows([
-                                ...workoutRows.slice(0, index),
-                                rowToAddWorkoutTo,
-                                ...workoutRows.slice(index+1)
-                              ]);
-                              // reset description to blank
-                              setDescriptionBox("");
-                            }
-                          }}
-                        >
-                          Enter Workout
-                        </button>
-                      </>
-                    ) : (
-                      // Switch which row to add new workout to
-                      <button className="btn btn-info" onClick={() => setAddIndex(index)}>Add Workout</button>
-                    )}
-                  </td>
-                </div>
-              </tr>
-            );
-          })}
+            {/* Creating new array under row.workouts */}
+            {/* Iterating over row.workouts array --> created below!! */}
+            {row.workouts.map((workout, index) => {
+              return (
+                <div className=" homeWorkout__cell col">{workout}</div>
+              );
+            })}
 
-          <div>
-            {/* Create new workoutRow and workout list underneath */}
-            <input
-              type="text"
-              name="description"
-              value={descriptionHeader}
-              onChange={(e) => setDescriptionHeader(e.target.value)}
-            />
-            {/* Create new workoutRow and workout list underneath */}
-            <button
-              className="btn btn-primary"
-              onClick={() => {
-                // make sure descriptionHeader isn't empty
-                if (descriptionHeader.length) {
-                  // take current task list and add new header at end
-                  setWorkoutRows([
-                    ...workoutRows,
-                    {
-                      // ????? set typed info to description
-                      descriptionHeader: descriptionHeader,
-                      // Create workouts array
-                      workouts: [],
-                    },
-                  ]);
-                  // reset description to blank
-                  setDescriptionHeader("");
-                }
-              }}
-            >
-              Enter Header Cell
-            </button>
+            <div className="homeWorkout__subDel height col-1">
+              {/* delete whole workoutRow index */}
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteWorkoutRow(index)}
+              >
+                Delete Row
+              </button>
+            </div>
+            <div className="col-1 text height">
+              {/* change add workout box depending on what index has been clicked */}
+              {index == addIndex ? (
+                <>
+                  {/* Add workouts to specific row */}
+                  <input
+                    className="form-control input-lg"
+                    type="text"
+                    name="description"
+                    value={descriptionBox}
+                    onChange={(e) => setDescriptionBox(e.target.value)}
+                  />
+
+                  {/* add workout based off of input  */}
+                  <button
+                    className="btn btn-success height"
+                    onClick={() => {
+                      // make sure new workout isn't empty
+                      if (descriptionBox.length) {
+                        //
+                        let rowToAddWorkoutTo = workoutRows.slice(index)[0];
+                        //push WorkoutRows information to descriptionBox
+                        rowToAddWorkoutTo.workouts.push(descriptionBox);
+                        // Place new workout at end of array ---- take workoutRows Array first index
+                        setWorkoutRows([
+                          ...workoutRows.slice(0, index),
+                          rowToAddWorkoutTo,
+                          ...workoutRows.slice(index + 1),
+                        ]);
+                        // reset description to blank
+                        setDescriptionBox("");
+                      }
+                    }}
+                  >
+                    Enter Workout
+                  </button>
+                </>
+              ) : (
+                // Switch which row to add new workout to
+                <button
+                  className="btn btn-info"
+                  onClick={() => setAddIndex(index)}
+                >
+                  Add Workout
+                </button>
+              )}
+            </div>
           </div>
-        </tbody>
-      </table>
+        );
+      })}
+
+      <div>
+        {/* Create new workoutRow and workout list underneath */}
+        <input
+          type="text"
+          name="description"
+          value={descriptionHeader}
+          onChange={(e) => setDescriptionHeader(e.target.value)}
+        />
+        {/* Create new workoutRow and workout list underneath */}
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            // make sure descriptionHeader isn't empty
+            if (descriptionHeader.length) {
+              // take current task list and add new header at end
+              setWorkoutRows([
+                ...workoutRows,
+                {
+                  // ????? set typed info to description
+                  descriptionHeader: descriptionHeader,
+                  // Create workouts array
+                  workouts: [],
+                },
+              ]);
+              // reset description to blank
+              setDescriptionHeader("");
+            }
+          }}
+        >
+          Enter Header Cell
+        </button>
+      </div>
     </div>
   );
 }
